fca735754ab49c87c43e2e5829718f0dd791ad34
Elizaveta Ozerova <e.ozerova@adsterra.com>
2021-06-20 12:57:49 +0300

    добавлен конфиг и deploy.sh для размещения на github pages

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lisaozerova/day-times.git master:gh-pages
 
 cd -
